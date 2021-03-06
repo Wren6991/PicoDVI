83021d19fcc97435065b6777ba8880d7172f60d6
Mike Bell <mdb036@gmail.com>
2021-03-06 16:15:27 +0000

    Add palette encoder
    (cherry picked from commit c30692ee75b8a2e3aaf4d7d5b809c267f9066aed)

--- software/libdvi/tmds_encode.c
+++ software/libdvi/tmds_encode.c
@@ -162,3 +162,71 @@ void __not_in_flash_func(tmds_encode_data_channel_fullres_16bpp)(const uint32_t 
 	interp_restore(interp1_hw, &interp1_save);
 #endif
 }
+
+// This takes a 16-bit (RGB 565) colour palette and makes palettes of TMDS symbols suitable
+// for performing fullres encode.
+// The TMDS palette buffer should be 6 * n_palette words long.
+// n_palette must be a multiple of 2 <= 64.  I've only tested 64 at present.
+void tmds_setup_palette_symbols(const uint16_t *palette, uint32_t *tmds_palette, size_t n_palette) {
+	uint32_t* tmds_palette_blue = tmds_palette;
+	uint32_t* tmds_palette_green = tmds_palette + 2 * n_palette;
+	uint32_t* tmds_palette_red = tmds_palette + 4 * n_palette;
+	for (int i = 0; i < n_palette; ++i) {
+		uint16_t blue = (palette[i] << 1) & 0x3e;
+		uint16_t green = (palette[i] >> 5) & 0x3f;
+		uint16_t red = (palette[i] >> 10) & 0x3e;
+		tmds_palette_blue[i] = tmds_table_fullres_x[blue];
+		tmds_palette_blue[i + n_palette] = tmds_table_fullres_x[n_palette + blue];
+		tmds_palette_green[i] = tmds_table_fullres_x[green];
+		tmds_palette_green[i + n_palette] = tmds_table_fullres_x[n_palette + green];
+		tmds_palette_red[i] = tmds_table_fullres_x[red];
+		tmds_palette_red[i + n_palette] = tmds_table_fullres_x[n_palette + red];
+	}
+}
+
+// Encode palette data for all 3 channels.
+// pixbuf is an array of n_pix 8-bit wide pixels containing palette values (32-bit word aligned)
+// The palette entry is taken from be the 6 MSBs of the pixel value (bottom 2 bits are ignored)
+// tmds_palette is a palette of TMDS symbols produced by tmds_setup_palette_symbols
+// symbuf is 3*n_pix 32-bit words, this function writes the symbol values for each of the channels to it.
+void __not_in_flash_func(tmds_encode_palette_data)(const uint32_t *pixbuf, const uint32_t *tmds_palette, uint32_t *symbuf, size_t n_pix, uint32_t palette_bits) {
+	uint core = get_core_num();
+#if !TMDS_FULLRES_NO_INTERP_SAVE
+	interp_hw_save_t interp0_save, interp1_save;
+	interp_save(interp0_hw, &interp0_save);
+	interp_save(interp1_hw, &interp1_save);
+#endif
+
+	interp0_hw->base[2] = (uint32_t)tmds_palette;
+	interp1_hw->base[2] = (uint32_t)tmds_palette;
+	interp0_hw->ctrl[0] = 0x02800040u | ((palette_bits + 2) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
+	interp1_hw->ctrl[0] = 0x02800048u | ((palette_bits + 2) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
+	interp0_hw->ctrl[1] = 0x00002117u;
+	interp1_hw->ctrl[1] = 0x00002117u;
+	if (core) {
+		tmds_palette_encode_loop_x(pixbuf, symbuf, n_pix);
+
+		interp0_hw->base[2] = (uint32_t)(tmds_palette + (2 << palette_bits));
+		interp1_hw->base[2] = (uint32_t)(tmds_palette + (2 << palette_bits));
+		tmds_palette_encode_loop_x(pixbuf, symbuf + n_pix, n_pix);
+
+		interp0_hw->base[2] = (uint32_t)(tmds_palette + (4 << palette_bits));
+		interp1_hw->base[2] = (uint32_t)(tmds_palette + (4 << palette_bits));
+		tmds_palette_encode_loop_x(pixbuf, symbuf + n_pix * 2, n_pix);
+	} else {
+		tmds_palette_encode_loop_y(pixbuf, symbuf, n_pix);
+
+		interp0_hw->base[2] = (uint32_t)(tmds_palette + (2 << palette_bits));
+		interp1_hw->base[2] = (uint32_t)(tmds_palette + (2 << palette_bits));
+		tmds_palette_encode_loop_y(pixbuf, symbuf + n_pix, n_pix);
+
+		interp0_hw->base[2] = (uint32_t)(tmds_palette + (4 << palette_bits));
+		interp1_hw->base[2] = (uint32_t)(tmds_palette + (4 << palette_bits));
+		tmds_palette_encode_loop_y(pixbuf, symbuf + n_pix * 2, n_pix);
+	}
+
+#if !TMDS_FULLRES_NO_INTERP_SAVE
+	interp_restore(interp0_hw, &interp0_save);
+	interp_restore(interp1_hw, &interp1_save);
+#endif
+}
