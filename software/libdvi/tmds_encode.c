a281e8a24190c19b74939a0c4954fb71e44c1b35
Mike Bell <mdb036@gmail.com>
2021-03-07 00:27:13 +0000

    Up to 256 colour palettes
    (cherry picked from commit 86e0e5d7dd11020d01f167b8cad571391de56aee)

--- software/libdvi/tmds_encode.c
+++ software/libdvi/tmds_encode.c
@@ -166,7 +166,7 @@ void __not_in_flash_func(tmds_encode_data_channel_fullres_16bpp)(const uint32_t 
 // This takes a 16-bit (RGB 565) colour palette and makes palettes of TMDS symbols suitable
 // for performing fullres encode.
 // The TMDS palette buffer should be 6 * n_palette words long.
-// n_palette must be a multiple of 2 <= 64.  I've only tested 64 at present.
+// n_palette must be a power of 2 <= 256.
 void tmds_setup_palette_symbols(const uint16_t *palette, uint32_t *tmds_palette, size_t n_palette) {
 	uint32_t* tmds_palette_blue = tmds_palette;
 	uint32_t* tmds_palette_green = tmds_palette + 2 * n_palette;
@@ -176,17 +176,16 @@ void tmds_setup_palette_symbols(const uint16_t *palette, uint32_t *tmds_palette,
 		uint16_t green = (palette[i] >> 5) & 0x3f;
 		uint16_t red = (palette[i] >> 10) & 0x3e;
 		tmds_palette_blue[i] = tmds_table_fullres_x[blue];
-		tmds_palette_blue[i + n_palette] = tmds_table_fullres_x[n_palette + blue];
+		tmds_palette_blue[i + n_palette] = tmds_table_fullres_x[64 + blue];
 		tmds_palette_green[i] = tmds_table_fullres_x[green];
-		tmds_palette_green[i + n_palette] = tmds_table_fullres_x[n_palette + green];
+		tmds_palette_green[i + n_palette] = tmds_table_fullres_x[64 + green];
 		tmds_palette_red[i] = tmds_table_fullres_x[red];
-		tmds_palette_red[i + n_palette] = tmds_table_fullres_x[n_palette + red];
+		tmds_palette_red[i + n_palette] = tmds_table_fullres_x[64 + red];
 	}
 }
 
 // Encode palette data for all 3 channels.
 // pixbuf is an array of n_pix 8-bit wide pixels containing palette values (32-bit word aligned)
-// The palette entry is taken from be the 6 MSBs of the pixel value (bottom 2 bits are ignored)
 // tmds_palette is a palette of TMDS symbols produced by tmds_setup_palette_symbols
 // symbuf is 3*n_pix 32-bit words, this function writes the symbol values for each of the channels to it.
 void __not_in_flash_func(tmds_encode_palette_data)(const uint32_t *pixbuf, const uint32_t *tmds_palette, uint32_t *symbuf, size_t n_pix, uint32_t palette_bits) {
@@ -199,10 +198,10 @@ void __not_in_flash_func(tmds_encode_palette_data)(const uint32_t *pixbuf, const
 
 	interp0_hw->base[2] = (uint32_t)tmds_palette;
 	interp1_hw->base[2] = (uint32_t)tmds_palette;
-	interp0_hw->ctrl[0] = 0x02800040u | ((palette_bits + 2) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
-	interp1_hw->ctrl[0] = 0x02800048u | ((palette_bits + 2) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
-	interp0_hw->ctrl[1] = 0x00002117u;
-	interp1_hw->ctrl[1] = 0x00002117u;
+	interp0_hw->ctrl[0] = 0x40u | ((palette_bits + 1) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
+	interp1_hw->ctrl[0] = 0x48u | ((palette_bits + 1) << SIO_INTERP0_CTRL_LANE0_MASK_MSB_LSB);
+	interp0_hw->ctrl[1] = (29 - palette_bits) + 0x420 * (palette_bits + 2);
+	interp1_hw->ctrl[1] = (29 - palette_bits) + 0x420 * (palette_bits + 2);
 	if (core) {
 		tmds_palette_encode_loop_x(pixbuf, symbuf, n_pix);
 
