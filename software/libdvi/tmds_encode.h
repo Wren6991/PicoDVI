af7f430aff9ed7622f2eba0771e44862e5ea0fd0
Mike Bell <mike@mercuna.com>
2021-03-19 12:53:26 +0000

    Encode symbols for palette rather than relying on hardcoded values.  Allows 24-bit colour values.

--- software/libdvi/tmds_encode.h
+++ software/libdvi/tmds_encode.h
@@ -9,6 +9,7 @@ void tmds_encode_data_channel_16bpp(const uint32_t *pixbuf, uint32_t *symbuf, si
 void tmds_encode_data_channel_8bpp(const uint32_t *pixbuf, uint32_t *symbuf, size_t n_pix, uint channel_msb, uint channel_lsb);
 void tmds_encode_data_channel_fullres_16bpp(const uint32_t *pixbuf, uint32_t *symbuf, size_t n_pix, uint channel_msb, uint channel_lsb);
 void tmds_setup_palette_symbols(const uint16_t *palette, uint32_t *symbuf, size_t n_palette);
+void tmds_setup_palette24_symbols(const uint32_t *palette, uint32_t *symbuf, size_t n_palette);
 void tmds_encode_palette_data(const uint32_t *pixbuf, const uint32_t *tmds_palette, uint32_t *symbuf, size_t n_pix, uint32_t palette_bits);
 
 // Functions from tmds_encode.S
