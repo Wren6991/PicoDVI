5034a9abff35918f28919da8b8bbbb033675a7c6
Luke Wren <wren6991@gmail.com>
2021-05-18 12:39:06 +0100

    Fixups for SDK 5384b6b: rename lock core member, workaround for scope change of default bs2 file

--- software/libdvi/util_queue_u32_inline.h
+++ software/libdvi/util_queue_u32_inline.h
@@ -18,38 +18,38 @@ static inline uint16_t _queue_inc_index_u32(queue_t *q, uint16_t index) {
 
 static inline bool queue_try_add_u32(queue_t *q, void *data) {
     bool success = false;
-    uint32_t flags = spin_lock_blocking(q->lock);
+    uint32_t flags = spin_lock_blocking(q->core.spin_lock);
     if (queue_get_level_unsafe(q) != q->element_count) {
         ((uint32_t*)q->data)[q->wptr] = *(uint32_t*)data;
         q->wptr = _queue_inc_index_u32(q, q->wptr);
         success = true;
     }
-    spin_unlock(q->lock, flags);
+    spin_unlock(q->core.spin_lock, flags);
     if (success) __sev();
     return success;
 }
 
 static inline bool queue_try_remove_u32(queue_t *q, void *data) {
     bool success = false;
-    uint32_t flags = spin_lock_blocking(q->lock);
+    uint32_t flags = spin_lock_blocking(q->core.spin_lock);
     if (queue_get_level_unsafe(q) != 0) {
         *(uint32_t*)data = ((uint32_t*)q->data)[q->rptr];
         q->rptr = _queue_inc_index_u32(q, q->rptr);
         success = true;
     }
-    spin_unlock(q->lock, flags);
+    spin_unlock(q->core.spin_lock, flags);
     if (success) __sev();
     return success;
 }
 
 static inline bool queue_try_peek_u32(queue_t *q, void *data) {
     bool success = false;
-    uint32_t flags = spin_lock_blocking(q->lock);
+    uint32_t flags = spin_lock_blocking(q->core.spin_lock);
     if (queue_get_level_unsafe(q) != 0) {
         *(uint32_t*)data = ((uint32_t*)q->data)[q->rptr];
         success = true;
     }
-    spin_unlock(q->lock, flags);
+    spin_unlock(q->core.spin_lock, flags);
     return success;
 }
 
